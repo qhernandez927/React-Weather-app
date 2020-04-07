2ffab837d1e4684319094a2f1b0022324751b934
qhernandez927 <quinnh2791@gmail.com>
2020-04-07 17:06:13 -0500

    added date and template literal. styling started

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", 
+    "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -15,6 +29,16 @@ function App() {
           className="search-bar"
           placeholder="Search..."
           ></input>
+        </div>
+        <div className = "location-box">
+          <div className = "location"> New York City, US</div>
+          <div className = "date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className = "weather-box">
+          <div className = "temp">
+            15°c
+          </div>
+          <div className = "weather">Sunny</div> 
         </div>
       </main>
     </div>
