5f45e08b164eaab12c62b9f9c909155e9ebd9473
qhernandez927 <quinnh2791@gmail.com>
2020-04-07 15:53:46 -0500

    first commit for weather app. background image and search bar complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const api = {
+  key: "bc1a4bb5afd72e257b1c0b03da202201",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className = "search-box">
+          <input 
+          type = "text"
+          className="search-bar"
+          placeholder="Search..."
+          ></input>
+        </div>
+      </main>
     </div>
   );
 }
