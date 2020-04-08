23add636e1aadac7e9416e07afc9bd2f74c6f06e
qhernandez927 <quinnh2791@gmail.com>
2020-04-07 19:21:33 -0500

    fixed ease=out bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const api = {
 }
 
 function App() {
-
+  
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
@@ -55,7 +55,7 @@ function App() {
         {(typeof weather.main != "undefined") ? (
         <div>
           <div className = "location-box">
-            <div className = "location">{weather.name}, {weather.sys.country}</div>
+            <div className = "location">{weather.name},{weather.sys.country}</div>
             <div className = "date">{dateBuilder(new Date())}</div>
           </div>
           <div className = "weather-box">
